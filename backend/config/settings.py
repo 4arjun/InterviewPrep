d565a5eb55c71327939b136be3f8c584cb0d13e6
4arjun <arjunajith440@gmail.com>
2025-06-18 00:24:48 +0530

    connectivity

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,10 +85,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Arjunajith@123',
+        'HOST': 'db.eojuvvwdlikwdngkdzkm.supabase.co',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'prefer',
+        }
     }
 }
+
+
 
 
 # Password validation
@@ -133,5 +143,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
