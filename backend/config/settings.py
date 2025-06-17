05c5c358e8b715452a382e7705e58513888ddf05
4arjun <arjunajith440@gmail.com>
2025-06-18 00:29:12 +0530

    fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -145,4 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173"
+    "https://app-4510091842.europe-west1.run.app",
+
 ]
